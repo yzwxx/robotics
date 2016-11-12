4156068c651539c33b178a519c5e1143b0061b62
robot <yaozhongwei0131@163.com>
2016-11-12 18:27:11 +0000

    Monte Carlo

---
+++ prac-mlc/motor_position.py
@@ -0,0 +1,40 @@
+import brickpi
+import time
+
+interface=brickpi.Interface()
+interface.initialize()
+
+motors = [0,1]
+
+interface.motorEnable(motors[0])
+interface.motorEnable(motors[1])
+
+motorParams = interface.MotorAngleControllerParameters()
+motorParams.maxRotationAcceleration = 6.0
+motorParams.maxRotationSpeed = 12.0
+motorParams.feedForwardGain = 255/20.0
+motorParams.minPWM = 18.0
+motorParams.pidParameters.minOutput = -255
+motorParams.pidParameters.maxOutput = 255
+motorParams.pidParameters.k_p = 100.0
+motorParams.pidParameters.k_i = 0.0
+motorParams.pidParameters.k_d = 0.0
+
+interface.setMotorAngleControllerParameters(motors[0],motorParams)
+interface.setMotorAngleControllerParameters(motors[1],motorParams)
+
+while True:
+	angle = float(input("Enter a angle to rotate (in radians): "))
+
+	interface.increaseMotorAngleReferences(motors,[angle,angle])
+
+	while not interface.motorAngleReferencesReached(motors) :
+		motorAngles = interface.getMotorAngles(motors)
+		if motorAngles :
+			print "Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0]
+		time.sleep(0.1)
+
+	print "Destination reached!"
+	
+
+interface.terminate()
